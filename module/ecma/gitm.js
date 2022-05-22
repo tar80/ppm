ce70c46abd9feff17cd10723dc831c0d2f9c8158
tar80 <tar80master@gmail.com>
2022-05-22 17:04:22 +0900

    Add ecma/gtim.js

---
+++ module/ecma/gitm.js
@@ -0,0 +1,48 @@
+﻿(() => {
+  'use strict';
+  const obj = PPx.CreateObject('Scripting.FileSystemObject');
+  return {
+    root: (pwd) => {
+      let hasGit;
+      if (pwd.indexOf('aux:') === 0) {
+        pwd = pwd.replace(/aux:([\\/])*[SM]_[^\\]*\\(.*)?/, (_p0, p1, p2) =>
+          typeof p1 === 'undefined' ? p2 : ''
+        );
+      }
+      if (pwd !== '') {
+        pwd = obj.GetFolder(pwd);
+        do {
+          hasGit = obj.BuildPath(pwd, '.git');
+          if (obj.FolderExists(hasGit)) return String(pwd);
+          pwd = pwd.ParentFolder;
+          if (pwd === null || pwd.IsRootFolder) break;
+        } while (!pwd.IsRootFolder);
+      }
+      return '';
+    },
+    branch: (root) => {
+      const hasHead = obj.BuildPath(root, '.git\\head');
+      if (obj.FileExists(hasHead)) {
+        st.Open;
+        st.Type = 2;
+        st.Charset = 'UTF-8';
+        st.LoadFromFile(hasHead);
+        const data = st.ReadText(-1);
+        st.close;
+        return ~data.indexOf('ref:') ? data.slice(16, -1) : data.slice(0, -1);
+      }
+    },
+    head: (root, branch) => {
+      const hasHeads = obj.BuildPath(root, '.git\\refs\\heads\\' + branch);
+      if (obj.FileExists(hasHeads)) {
+        st.Open;
+        st.Type = 2;
+        st.Charset = 'UTF-8';
+        st.LoadFromFile(hasHeads);
+        const data = st.ReadText(-1);
+        st.close;
+        return data.slice(0, -1);
+      }
+    }
+  };
+})();
