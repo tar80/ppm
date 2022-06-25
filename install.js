d2b63b282f5fddc76c7ed7c8cb1a94f8f0a8e9bd
tar80 <tar80master@gmail.com>
2022-06-25 13:54:39 +0900

    Fix *ppmRestoreUnregのバグを修正
    ;[end]以降が全て削除されてしまったいたので修正

--- install.js
+++ install.js
@@ -2,14 +2,14 @@
 /**
  * Install ppx-plugin-manager
  *
- * @version 0.72
+ * @version 0.73
  * @arg 0 Install process. 0:user | 1:developer | 2:update
  */
 
 /* constants */
 var PPX_VERSION = 18403;
 var SCRIPT_VERSION = 18;
-var PPM_VERSION = 0.72;
+var PPM_VERSION = 0.73;
 var NEWLINE_CODE = 'crlf';
 
 // Require modules
