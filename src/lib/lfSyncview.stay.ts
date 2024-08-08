9b7362cdabbb9400bce2a98feec20509466c0411
tar80 <tar80master@gmail.com>
2024-08-08 21:52:20 +0900

    fix: 仮想エントリ連動ビューの調整をした

--- src/lib/lfSyncview.stay.ts
+++ src/lib/lfSyncview.stay.ts
@@ -12,7 +12,9 @@ import debug from '@ppmdev/modules/debug.ts';
 
 const STAYMODE_ID = 80000;
 const EVENT_LABEL = 'ppm_lfsyncview';
-const cache = {ppvid: '', close: false, debug: false};
+
+type Cache = {ppvid: string; close: boolean; debug: boolean};
+const cache = {} as Cache;
 
 const main = (): void => {
   const [keyspec, style, closeppv, debugMode] = safeArgs('@\\Y', 0, 0, '0');
@@ -26,38 +28,46 @@ const main = (): void => {
 
   cache.close = closeppv === 1;
   cache.debug = debugMode === 'DEBUG';
+
   cache.debug && debugMsg('start');
 
   startPPv(style);
   PPx.StayMode = STAYMODE_ID;
   ppx_Syncview();
-  PPx.Execute(`*linecust ${EVENT_LABEL},KC_main:SELECTEVENT,*if ("%n"=="%%n")%%:*script ":${STAYMODE_ID},ppx_Syncview"`);
 };
 
 const ppx_Syncview = (): void => {
+  if (isEmptyStr(PPx.Extract(`%N${cache.ppvid}`))) {
+    ppx_resume();
+
+    return;
+  }
+
   PPx.Execute(`*execute ${cache.ppvid},%%J"%*name(DC,"%*comment")"`);
-  isEmptyStr(PPx.Extract(`%N${cache.ppvid}`)) && ppx_resume();
 };
 
 const ppx_resume = () => {
   PPx.StayMode = 0;
   PPx.SyncView = 0;
-  PPx.Execute(`*linecust ${EVENT_LABEL},KC_main:SELECTEVENT,`);
+  PPx.Execute(selectevent(''));
   cache.close && PPx.Execute(`*closeppx ${cache.ppvid}`);
   cache.debug && debugMsg('discard');
 };
 
 const startPPv = (style: number) => {
   const option = style === 1 ? '-popup:%~N' : '';
-  PPx.Execute(`%Oa *ppv ${option}`);
+  PPx.Execute(`%Oai *ppv ${option}`);
   PPx.Sleep(100);
   cache.ppvid = PPx.Extract('%*extract(V,"%%n")');
 
   if (style === 2) {
     PPx.Execute(`*capturewindow ${cache.ppvid} -pane:~ -selectnoactive`);
   }
+
+  PPx.Execute(selectevent(`*if ("%n"=="%%n")%%:*script ":${STAYMODE_ID},ppx_Syncview"`));
 };
 
+const selectevent = (cmdline: string): string => `*linecust ${EVENT_LABEL},KC_main:SELECTEVENT,${cmdline}`;
 const debugMsg = (msg: string): void => PPx.linemessage(`[DEBUG] lfSyncView ${msg}`);
 
 main();
